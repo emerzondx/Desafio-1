4b07c3e0708ed1dc1894bfc4321e3f66f4675b8e
Emerzon Dolarea <emerzondx@gmial.com>
2022-09-27 13:56:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function encrypt() {
+    var message = document.getElementById('encrypted').value.toLowerCase();
+    var encrypted = frase.replace(/e/img, "enter");
+    var encrypted = frase.replace(/o/img, "ober");
+    var encrypted = frase.replace(/i/img, "imes");
+    var encrypted = frase.replace(/a/img, "ai");
+    var encrypted = frase.replace(/u/img, "ufat");
+
+}
