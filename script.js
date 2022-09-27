824bfaac14427e17d2f432661f12e4421bf468ac
Emerzon Dolarea <emerzondx@gmail.com>
2022-09-27 15:57:27 -0300

    tets1

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function encrypt() {
-    var message = document.getElementById('encrypted').value.toLowerCase();
-    var encrypted = frase.replace(/e/img, "enter");
-    var encrypted = frase.replace(/o/img, "ober");
-    var encrypted = frase.replace(/i/img, "imes");
-    var encrypted = frase.replace(/a/img, "ai");
-    var encrypted = frase.replace(/u/img, "ufat");
 
-}
+
+
+
+
+// function encrypt() {
+//     var message = document.getElementById('encrypted').value.toLowerCase();
+//     var encrypted = frase.replace(/e/img, "enter");
+//     var encrypted = frase.replace(/o/img, "ober");
+//     var encrypted = frase.replace(/i/img, "imes");
+//     var encrypted = frase.replace(/a/img, "ai");
+//     var encrypted = frase.replace(/u/img, "ufat");
+
+// }
